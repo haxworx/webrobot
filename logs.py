d217e6e327b80a55a37327d8d41844cc429dd094
Alastair Poole <netstar@gmail.com>
2022-02-23 17:46:10 +0000

    logs: logging to db fails then terminate.
    Create a file and write some debugging information to it.

--- logs.py
+++ logs.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from logging import StreamHandler
+import mysql.connector
+from mysql.connector import errorcode
 
 class DatabaseHandler(StreamHandler):
     def __init__(self, crawler):
@@ -16,5 +18,15 @@ class DatabaseHandler(StreamHandler):
         SQL = "INSERT INTO tbl_app_log (time_stamp, `crawler_name`, `hostname`, `ip_address`, level_number, level_name, message) VALUES (NOW(), %s, %s, %s, %s, %s, %s)"
         cursor = self.cnx.cursor()
         data = (self.crawler.name, self.crawler.hostname, self.crawler.ip_address, level_number, level_name, msg)
-        cursor.execute(SQL, data)
-        self.cnx.commit()
+        try:
+            cursor.execute(SQL, data)
+            self.cnx.commit()
+        except mysql.connector.Error as e:
+            with open("something_really_bad_happened.txt", "w") as f:
+                    f.write("Logging failed:\n"
+                            "\tError code: {}\n"
+                            "\tSQLSTATE:   {}\n"
+                            "\tMessage:    {}\n" . format(e.errno, e.sqlstate, e.msg))
+            sys.exit(2)
+
+        cursor.close()
