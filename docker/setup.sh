043a0373944827ec8d5472836cea8280cd8cd53c
Alastair Poole <netstar@gmail.com>
2022-03-06 07:29:46 +0000

    docker: try to run this on setup

---
+++ docker/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get -y upgrade
+apt-get -y install python3 pip git
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+python3 -m pip install mysql.connector
+mkdir -p /opt/crawler
+cd /opt/crawler
+git clone https://github.com/haxworx/webrobot --depth 1
+cd webrobot
+python3 make_config.py datacentre crawl test password
+python3 start_robot.py
---
+++ make_config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) != 5:
+    print("{} <db_host> <db_name> <db_user> <db_pass>" . format(sys.argv[0]))
+    sys.exit(1)
+
+db_host = sys.argv[1]
+db_name = sys.argv[2]
+db_user = sys.argv[3]
+db_pass = sys.argv[4]
+
+with open('config.template', 'r') as f:
+    text = f.read()
+    text = text . format(db_host, db_name, db_user, db_pass)
+    with open('config.txt', 'w') as of:
+        of.write(text)
