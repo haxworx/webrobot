c3bc90ebe3e8800d66c8eb98e59cdebb1feb0ce6
Alastair Poole <netstar@gmail.com>
2022-03-15 15:50:46 +0000

    docker: update bootstrap

--- docker/run.sh
+++ docker/run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [[ "$@" == "" ]]; then
-	echo "Missing parameter. Aborting."
+if [[ $# != 3 ]]; then
+	echo "Usage: $0 <host> <user-agent>"
 	exit 1
 fi
+
+HOST=$1
+AGENT=$2
 
 cd /opt/crawler/webrobot
 
 python3 make_config.py datacentre crawl test password
-python3 robot_start.py $1
+python3 robot_start.py \'$HOST\' \'$AGENT\'
