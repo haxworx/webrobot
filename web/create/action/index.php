4bcacc872a2a0b453dafe829adb4fdbce6c5c483
Alastair Poole <netstar@gmail.com>
2022-03-18 10:01:04 +0000

    create: fix regex.

--- web/create/action/index.php
+++ web/create/action/index.php
@@ -18,7 +18,7 @@ require_once 'lib/Config.php';
 
 function valid_address($address)
 {
-	if (preg_match('/^http|https:\/\/+((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/', strtolower($address))) {
+	if (preg_match('/^(http|https):\/\/((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/', strtolower($address))) {
 		return true;
 	}
 	return false;
