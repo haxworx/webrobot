a5c74ae122b09fbedeb6ff630a071eea5a11f83e
Alastair Poole <netstar@gmail.com>
2022-03-19 08:29:11 +0000

    confirm: add a confirm redirect.

---
+++ web/confirm/index.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once '../lib/common.php';
+require_once 'lib/Database.php';
+require_once 'lib/Twig.php';
+
+$params = [];
+
+foreach ($_GET as $key => $value) {
+	$params[] = ['key' => $key, 'value' => $value];
+}
+
+if (!isset($_GET['action']) || empty($_GET['action'])) {
+	header("Location: /");
+	return;
+}
+
+$action = $_GET['action'];
+if (!preg_match('/^[a-zA-Z0-9]+$/', $action)) {
+	header("Location: /");
+	return;
+}
+
+$template = $twig->load('confirm.html.twig');
+
+echo $template->render(['action' => $action, 'params' => $params]);
+
+?>
