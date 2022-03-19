12cb280a480ea20f687cfee7a2d65f8ece319684
Alastair Poole <netstar@gmail.com>
2022-03-19 11:38:26 +0000

    stream: use config for MQTT stream.

--- web/live/stream.php
+++ web/live/stream.php
@@ -1,28 +1,39 @@
 <?php
 
 require_once '../lib/common.php';
+require_once 'lib/Config.php';
 require 'vendor/autoload.php';
+
+# Create a Stream of our MQTT logging in real-time.
+
+$config = new Config();
+
+$server    = $config->settings['mqtt']['host'];
+$port      = intval($config->settings['mqtt']['port']);
+$topic     = $config->settings['mqtt']['topic'];
+$client_id = "insert_something_session_like_here";
+
 header("Content-Type: text/plain");
 header('Cache-Control: no-cache');
-$server = 'datacentre';
-$port   = 1883;
-$clientId = 'insert_here';
 
 for ($i = 0; $i < 1024; $i++) {
-  echo ' ';
+	echo ' ';
 }
+
 ob_flush();
 flush();
-$mqtt = new \PhpMqtt\Client\MqttClient($server, $port, $clientId);
+
+$mqtt = new \PhpMqtt\Client\MqttClient($server, $port, $client_id);
 $mqtt->connect();
-$mqtt->subscribe('testing', function ($topic, $message) {
-   echo "$topic: $message\n";
-   ob_flush();
+$mqtt->subscribe($topic, function ($topic, $message) {
+	$message = rtrim($message, $characters = " \n\r\t\v\x00");
+	echo "$topic: $message\n";
+	ob_flush();
 }, 0);
 
 $mqtt->loop(true);
+
 ob_end_flush();
 $mqtt->disconnect();
-
 
 ?>
