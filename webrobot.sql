dabad7cab7e99314e2597ea635e34b7b41fe89c5
Alastair Poole <netstar@gmail.com>
2022-05-05 11:05:56 +0100

    webrobot: bad.

--- webrobot.sql
+++ webrobot.sql
@@ -236,7 +236,7 @@ CREATE TABLE `user` (
 -- Dump completed on 2022-05-04 17:43:14
 
 
-INSERT INTO `tbl_content_types` (`content_type`, `description`)
+INSERT INTO `content_types` (`content_type`, `description`)
 VALUES
     ('text/plain', 'Text'),
     ('text/html', 'HTML'),
@@ -245,7 +245,7 @@ VALUES
     ('text/xml', 'Text XML'),
     ('application/json', 'Application JSON');
 
-INSERT INTO `tbl_global_settings`
+INSERT INTO `global_settings`
     (`time_stamp`, `in_use`, `max_crawlers`, `debug`, `docker_image`, `mqtt_host`, `mqtt_port`, `mqtt_topic`)
     VALUES
     (NOW(), true, 5, true, 'spiderz', 'datacentre', 1883, 'testing');
