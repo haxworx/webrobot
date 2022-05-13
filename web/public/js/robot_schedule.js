6d528e664eec481173cd1273ed09f57c326ad30b
Alastair Poole <netstar@gmail.com>
2022-05-13 07:08:55 +0100

    delete: disable buttons on confirm.

--- web/public/js/robot_schedule.js
+++ web/public/js/robot_schedule.js
@@ -9,6 +9,10 @@ myModal.addEventListener('shown.bs.modal', function () {
 function removeCrawler(botId, token) {
     let spinner = document.getElementById('spinner');
     spinner.classList.remove('visually-hidden');
+    let save_button = document.getElementById('robot_schedule_save')
+    save_button.disabled = true;
+    let delete_button = document.getElementById('robot_schedule_delete');
+    delete_button.disabled = true;
 
     let xhr = new XMLHttpRequest();
     xhr.open('POST', '/robot/schedule/remove/' + botId, true);
