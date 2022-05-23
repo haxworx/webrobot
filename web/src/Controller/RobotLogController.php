8cd547652af34bbf3875e845c81d20587ab672f4
Alastair Poole <netstar@gmail.com>
2022-05-23 19:08:15 +0100

    form: remove dead non-form.
    This is dead code, purge it.

--- web/src/Controller/RobotLogController.php
+++ web/src/Controller/RobotLogController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\CrawlLog;
 use App\Entity\CrawlSettings;
-use App\Form\RobotLogType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
