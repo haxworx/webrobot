ccb84c89f8c60296c6c46a631a311bfbfa21112f
Alastair Poole <mail@alastairpoole.com>
2022-06-22 15:53:18 +0100

    notification: add bootstrap 5 notification API.
    Essentially append a notification to the main content with the
    message.
    
    We need this for messages on network error etc.

--- assets/controllers/notification.js
+++ assets/controllers/notification.js
@@ -2,15 +2,31 @@ const NOTIFICATION_URL = "/flashes";
 
 export class Notification {
     constructor(message){
+        this.messageText = message;
         this.message = {
-            message: message,
+            message: this.messageText,
         }
+    }
+
+    // Use Javascript to append Bootstrap 5 alert to our main content.
+    show() {
+        let container = document.querySelector('#main');
+        let div = document.createElement('div');
+        div.classList.add("alert", "alert-info", "alter-dismissible", "fade", "show");
+        div.innerHTML = this.messageText;
+        let button = document.createElement('button');
+        button.setAttribute('data-bs-dismiss', 'alert');
+        button.setAttribute('aria-label', 'Close');
+        button.classList.add("btn-close");
+        div.prepend(button);
+        container.prepend(div);
     }
 
     getUrl() {
         return NOTIFICATION_URL;
     }
 
+    // Send a POST request to use server-side generated notification.
     send() {
         let self = this;
         fetch(this.getUrl(), {
