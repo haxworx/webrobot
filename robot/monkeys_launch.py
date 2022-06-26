337a3cb8a5f5d89a7d6275c22e19c01445d6103b
Alastair Poole <mail@alastairpoole.com>
2022-06-26 18:44:57 +0100

    monkeys_launch: use a queue and a thread to launch.
    Playing here somewhat, but this shouldn't block duing Popen.

--- robot/monkeys_launch.py
+++ robot/monkeys_launch.py
@@ -10,16 +10,28 @@ import database
 import time
 import subprocess
 import signal
+import threading
+import queue
 from mysql.connector import errorcode
 from urllib.parse import urlparse
 from config import Config
 
 RETRY_MAX = 35
 
+queue = queue.Queue()
+
+def launcher():
+    while True:
+        bot_id = queue.get();
+        subprocess.Popen(["python3", "robot_start.py", str(bot_id)])
+        queue.task_done();
+
 def main():
     retry_count = 0
     config = Config(0)
     config.read_ini()
+
+    threading.Thread(target=launcher, daemon=True).start();
 
     # Ensure we don't end up with a million zombies.
     signal.signal(signal.SIGCHLD, signal.SIG_IGN)
@@ -42,9 +54,9 @@ def main():
                 print("Retry...{}" . format(retry_count), file=sys.stderr)
                 continue
 
-    seen = dict();
-    seen['ids'] = []
-    seen['time'] = "";
+    jobs = dict();
+    jobs['ids'] = []
+    jobs['current_time'] = "";
 
     while True:
 
@@ -59,25 +71,31 @@ def main():
             print("Error: ({}) STATE: ({}) Message: ({})" . format(e.errno, e.sqlstate, e.msg), file=sys.stderr)
             sys.exit(2)
 
+        # Keep track of current hour and minute.
+        # Reset our jobs array if the time has incremented.
         now = datetime.datetime.now().strftime("%H:%M")
-
-        if seen['time'] != now:
-            seen['ids'] = []
-        seen['time'] = now
+        if jobs['current_time'] != now:
+            jobs['ids'] = []
+        jobs['current_time'] = now
 
         for row in rows:
             bot_id, start_time = row[0], row[1]
-            start_time = (datetime.datetime.min + start_time).time()
-            timestamp = start_time.strftime("%H:%M")
-            if timestamp == seen['time'] and bot_id not in seen['ids']:
-                seen['ids'].append(bot_id)
-                subprocess.Popen(["python3", "robot_start.py", str(bot_id)])
+
+            # Check whether the timestamp from our row matches the time stamp
+            # of our jobs. If it matches and the id hasn't been dealt with
+            # then pass the bot_id to our thread and add it to the list of ids.
+            timestamp = (datetime.datetime.min + start_time).time().strftime("%H:%M")
+            if timestamp == jobs['current_time'] and bot_id not in jobs['ids']:
+                jobs['ids'].append(bot_id)
+                queue.put(bot_id)
 
         cnx.commit()
         cursor.close()
         time.sleep(1)
 
+    queue.join()
     cnx.close()
+
     return 0
 
 if __name__ == '__main__':
