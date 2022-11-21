b36bdecc2d01b4a10820a2a9eaf976de92c9d0f1
Alastair Poole <mail@alastairpoole.com>
2022-11-21 17:05:22 +0000

    login: detect caps lock.
    This adds an event listener on keydown to check for caps lock
    in email and password input fields.
    
    Currently appends or truncates a paragraph as indication.
    
    Change this to be visually more appealing.

--- assets/controllers/login_controller.js
+++ assets/controllers/login_controller.js
@@ -4,12 +4,19 @@ import { Notification } from './notification.js';
 import { Theme } from './theme.js';
 
 export default class extends Controller {
-    static targets = [ 'form' ];
+    static targets = [ 'form', 'capsWarning' ];
     static values = {
         token: String,
     }
 
     connect() {
+
+        let inputEmail = document.querySelector('#inputEmail');
+        let inputPassword = document.querySelector('#inputPassword');
+
+        this.detectCapsLock(inputEmail);
+        this.detectCapsLock(inputPassword);
+
         let form = this.formTarget;
         form.addEventListener('submit', (event) => {
             // In order to display a loading spinner on authentication,
@@ -19,6 +26,17 @@ export default class extends Controller {
             theme.displayLoadingSpinner();
             document.body.appendChild(form);
             form.style.display = "none";
+        });
+    }
+
+    detectCapsLock(input) {
+        input.addEventListener('keydown', (event) => {
+            let p = this.capsWarningTarget;
+            if (event.getModifierState("CapsLock")) {
+                p.textContent = "Caps Lock On";
+            } else {
+                p.textContent = "";
+            }
         });
     }
 }
