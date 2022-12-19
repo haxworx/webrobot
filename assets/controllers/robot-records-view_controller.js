af62be6f67002afabe1cfdc84ff611c4ccc245cc
Alastair Poole <mail@alastairpoole.com>
2022-12-19 16:17:59 +0000

    records: use iframe and AJAX for content.
    Use a pre tag and set innerHTML. Allows to easily parse
    document for search items and highlight them.
    
    WIP

--- assets/controllers/robot-records-view_controller.js
+++ assets/controllers/robot-records-view_controller.js
@@ -1,20 +1,35 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ['frame'];
+    static targets = ['pre'];
     static values = {
         botId: Number,
     }
 
     connect() {
-        const frame = this.frameTarget;
     }
 
     show(event) {
-        const frame = this.frameTarget;
+        const pre = this.preTarget;
         const recordId = event.params['id'];
         const botId = this.botIdValue || event.params['botId'];
-        frame.src = '/robot/records/show/' + botId + '/record/' + recordId;
+
+        fetch('/robot/records/download/' + botId + '/record/' + recordId, {
+            method: 'GET',
+        })
+        .then(response => response.text())
+        .then(data => {
+            pre.textContent = data;
+        })
+        .catch((error) => {
+            if (this.interval) {
+                clearInterval(this.interval);
+                this.interval = null;
+            }
+            console.error('Error:', error);
+            let notification = new Notification("There was a network error.", true);
+            notification.show();
+        });
     }
 
     download (event) {
